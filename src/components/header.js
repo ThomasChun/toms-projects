3c69301691e4e802ff6d2c478b3aa7aa425a5e0e
Thomas Chun <tchun2055@gmail.com>
2019-05-03 14:20:42 -0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="app-header">
+        TOMS PROJECT - Header Component
+      </header>
+    )
+  }
+}
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div className="app-main">
+        TOMS PROJECT - Main component
+      </div>
+    )
+  }
+}
